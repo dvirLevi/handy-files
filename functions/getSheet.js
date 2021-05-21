508bbc8249806287418ec69b271d0ca498077fd5
dvirLevi <dvirleviapp@gmail.com>
2021-05-21 11:53:46 +0300

    ass

--- functions/getSheet.js
+++ functions/getSheet.js
@@ -1,42 +1,82 @@
-const ifArrOrObjForEval = (str) => {
-    let ifArrOrObj = (arrOfObjStr) => {
-        const firstCharcter = arrOfObjStr.charAt(0)
-        const lastCharcter = arrOfObjStr.charAt(arrOfObjStr.length - 1)
-        if (firstCharcter === '[' && lastCharcter === ']' || firstCharcter === '{' && lastCharcter === '}') {
-            return true
-        }
-        return false
-    }
-    try {
-        if (!ifArrOrObj(str)) {
-            return false
-        }
-        eval(str)
-        return true
-    } catch {
-        return false
-    }
-}
+// const ifArrOrObjForEval = (str) => {
+//     let ifArrOrObj = (arrOfObjStr) => {
+//         const firstCharcter = arrOfObjStr.charAt(0)
+//         const lastCharcter = arrOfObjStr.charAt(arrOfObjStr.length - 1)
+//         if (firstCharcter === '[' && lastCharcter === ']' || firstCharcter === '{' && lastCharcter === '}') {
+//             return true
+//         }
+//         return false
+//     }
+//     try {
+//         if (!ifArrOrObj(str)) {
+//             return false
+//         }
+//         eval(str)
+//         return true
+//     } catch {
+//         return false
+//     }
+// }
 
-const googleSheetsToObj = (json, arrProp) => {
-    let arr = [];
-    for (let i in json.feed.entry) {
+// const googleSheetsToObj = (json, arrProp) => {
+//     let arr = [];
+//     for (let i in json.feed.entry) {
 
-        arr.push(
-            (() => {
-                let obj = {};
-                for (let x in arrProp) {
-                    let propOfObj = json.feed.entry[i][`gsx$${arrProp[x]}`]['$t']
-                    obj[arrProp[x]] = ifArrOrObjForEval(propOfObj) ? eval(propOfObj) : propOfObj
-                }
-                return obj
-            })()
-        )
-    }
-    return arr;
-}
+//         arr.push(
+//             (() => {
+//                 let obj = {};
+//                 for (let x in arrProp) {
+//                     let propOfObj = json.feed.entry[i][`gsx$${arrProp[x]}`]['$t']
+//                     obj[arrProp[x]] = ifArrOrObjForEval(propOfObj) ? eval(propOfObj) : propOfObj
+//                 }
+//                 return obj
+//             })()
+//         )
+//     }
+//     return arr;
+// }
 
 const getSheet = async (sheetId, arrProp) => {
+
+    const googleSheetsToObj = (json, arrProp) => {
+
+        const ifArrOrObjForEval = (str) => {
+            let ifArrOrObj = (arrOfObjStr) => {
+                const firstCharcter = arrOfObjStr.charAt(0)
+                const lastCharcter = arrOfObjStr.charAt(arrOfObjStr.length - 1)
+                if (firstCharcter === '[' && lastCharcter === ']' || firstCharcter === '{' && lastCharcter === '}') {
+                    return true
+                }
+                return false
+            }
+            try {
+                if (!ifArrOrObj(str)) {
+                    return false
+                }
+                eval(str)
+                return true
+            } catch {
+                return false
+            }
+        }
+
+        let arr = [];
+        for (let i in json.feed.entry) {
+    
+            arr.push(
+                (() => {
+                    let obj = {};
+                    for (let x in arrProp) {
+                        let propOfObj = json.feed.entry[i][`gsx$${arrProp[x]}`]['$t']
+                        obj[arrProp[x]] = ifArrOrObjForEval(propOfObj) ? eval(propOfObj) : propOfObj
+                    }
+                    return obj
+                })()
+            )
+        }
+        return arr;
+    }
+
     try {
         const response = await fetch(`https://spreadsheets.google.com/feeds/list/${sheetId}/od6/public/values?alt=json`);
         const json = await response.json();
